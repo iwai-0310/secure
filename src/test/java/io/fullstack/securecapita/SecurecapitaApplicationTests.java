b9273f3c97967a45b55fd94142d73f970f63da18
iwai-0310 <chazesingh@gmail.com>
2023-09-24 07:53:16 +0530

    create the backend  using spring io and added database config files

---
+++ src/test/java/io/fullstack/securecapita/SecurecapitaApplicationTests.java
@@ -0,0 +1,13 @@
+package io.fullstack.securecapita;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecurecapitaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
